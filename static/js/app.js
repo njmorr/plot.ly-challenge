157d19065908dfe2f2bcf88bd9c5e220389cb24a
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-28 22:42:19 -0500

    isolated demographic variables

--- static/js/app.js
+++ static/js/app.js
@@ -7,12 +7,12 @@
 
 // bar graph
 function drawBarGraph(sampleId) {
-    console.log(`drawBarGraph(${sampleId})`);
+    // console.log(`drawBarGraph(${sampleId})`);
 
     // reads data into a function; creates variables for bar chart
     d3.json("data/samples.json").then(data => {
         // console.log(data);
-        var samples =data.samples;
+        var samples = data.samples;
         var resultArray = samples.filter(s => s.id == sampleId);
         var result = resultArray[0];
         // console.log(result);
@@ -55,6 +55,49 @@ function drawBubbleChart(sampleId) {
 // meta data
 function showMetaData(sampleId) {
     console.log(`showMetaData(${sampleId})`);
+
+    // reads data into a function; creates variables for bar chart
+    d3.json("data/samples.json").then(data => {
+        // console.log(data);
+        var metadata = data.metadata;
+        var metadataArray = metadata.filter(s => s.id == sampleId)[0];
+        var idee = metadataArray.id;
+        var ethnicity = metadataArray.ethnicity;
+        var gender = metadataArray.gender;
+        var age = metadataArray.age;
+        var location = metadataArray.location;
+        var bbtype = metadataArray.bbtype;
+        var wfreq = metadataArray.wfreq;
+        console.log(idee, ethnicity, gender, age, location, bbtype, wfreq);
+
+        // var otu_ids = result.otu_ids;
+        // var otu_labels = result.otu_labels;
+        // var sample_values = result.sample_values;
+        // console.log(otu_labels);
+
+        // yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        // puts elements on bar chart into one variable
+        // var barData = {
+        //     x: sample_values.slice(0,10).reverse(),
+        //     y: yticks,
+        //     type: "bar",
+        //     text: otu_labels.slice(0,10).reverse(),
+        //     orientation: "h"
+        // }
+
+        // // puts bar chart variable into an array
+        // var barArray = [barData];
+
+        // // set layout for bar chart
+        // var barLayout = {
+        //     title: "Top 10 Bacteria Cultures Found",
+        //     margin: {t: 30, l: 150}
+        // }
+
+        // // plots bar chart
+        // Plotly.newPlot("bar", barArray, barLayout);
+    });
 }
 
 /////////////////////////////////////////////////////////
@@ -72,13 +115,13 @@ function optionChanged (newSampleId) {
 // initializing function
 
 function initDashboard() {
-    console.log("initDashboard function called");
+    // console.log("initDashboard function called");
 
     // Populate the dropdown menu
     var selector = d3.select("#selDataset");
 
     d3.json("data/samples.json").then(function(data){
-        console.log(data);
+        // console.log(data);
 
         var sampleNames = data.names;
 

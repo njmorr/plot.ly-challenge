0d0685eb3db22aefbdff939f79d86b22dba33eab
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-28 22:07:37 -0500

    bar graph function added

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,36 @@ function drawBarGraph(sampleId) {
     console.log(`drawBarGraph(${sampleId})`);
 
     d3.json("data/samples.json").then(data => {
-        console.log(data);
+        // console.log(data);
         var samples =data.samples;
         var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        // console.log(result);
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        // console.log(otu_labels);
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`); //TBD
+
+        var barData = {
+            x: sample_values.slice(0,10), //TBD
+            y: yticks,
+            type: bar,
+            text: otu_labels.slice(0,10),
+            orientation: "h"
+
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
     });
 }
 

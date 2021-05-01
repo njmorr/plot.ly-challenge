d06eff06cdf1db9234060a240ed4a2ed9bdc75d6
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-30 22:36:09 -0500

    building bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -51,44 +51,42 @@ function drawBarGraph(sampleId) {
 function drawBubbleChart(sampleId) {
     console.log(`drawBubbleChart(${sampleId})`);
 
-    // console.log(`drawBarGraph(${sampleId})`);
-
     // reads data into a function; creates variables for bar chart
-    // d3.json("data/samples.json").then(data => {
-    //     // console.log(data);
-    //     var samples = data.samples;
-    //     var resultArray = samples.filter(s => s.id == sampleId);
-    //     var result = resultArray[0];
-    //     // console.log(result);
+    d3.json("data/samples.json").then(data => {
+        // console.log(data);
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        // console.log(result);
 
-    //     var otu_ids = result.otu_ids;
-    //     var otu_labels = result.otu_labels;
-    //     var sample_values = result.sample_values;
-    //     // console.log(otu_labels);
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        console.log(sample_values);
 
-    //     yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+        yticks = otu_ids; //.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
 
-    //     // puts elements on bar chart into one variable
-    //     var barData = {
-    //         x: sample_values.slice(0,10).reverse(),
-    //         y: yticks,
-    //         type: "bar",
-    //         text: otu_labels.slice(0,10).reverse(),
-    //         orientation: "h"
-    //     }
+        // puts elements on bar chart into one variable
+        var bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {sample_values},
+            text: otu_labels
+        }
 
-    //     // puts bar chart variable into an array
-    //     var barArray = [barData];
+        // puts bar chart variable into an array
+        var bubbleArray = [bubbleData];
 
     //     // set layout for bar chart
-    //     var barLayout = {
+        // var bubbleLayout = {
     //         title: "Top 10 Bacteria Cultures Found",
     //         margin: {t: 30, l: 150}
     //     }
 
     //     // plots bar chart
     //     Plotly.newPlot("bar", barArray, barLayout);
-    // });
+    });
 }
 
 // meta data
